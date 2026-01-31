e41c3c92eb2252efb63463e7ec9325284bc2e133
Lucas Moura Alcantara <lucasmoura404@gmail.com>
2026-01-31 04:44:51 -0300

    feat: scripts do teste 2 no run_pipeline

--- scripts/run_pipeline.py
+++ scripts/run_pipeline.py
@@ -2,10 +2,16 @@ import subprocess
 import sys
 
 steps = [
+    # --- TESTE 1 ---
     "scripts/download_ans.py",
     "scripts/process_despesas.py",
     "scripts/enrich_operadoras.py",
     "scripts/consolidate_despesas.py",
+
+    # --- TESTE 2 ---
+    "scripts/validate_despesas.py",
+    "scripts/enrich_despesas.py",
+    "scripts/aggregate_despesas.py",
 ]
 
 for step in steps:
